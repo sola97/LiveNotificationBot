a3d830edb0c5233cd3cfb8b89348ca51fd7b1708
sola97 <sola97@qq.com>
2019-05-01 21:33:48 +0800

    修改频道不存在的判断逻辑

--- src/main/java/cn/sola97/bot/livenotification/Bot.java
+++ src/main/java/cn/sola97/bot/livenotification/Bot.java
@@ -10,21 +10,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
-public class Bot
-{       protected static final Logger logger = LoggerFactory.getLogger(Bot.class);
-        private static ScheduledExecutorService executor =  Executors.newSingleThreadScheduledExecutor();
-        private final EventWaiter waiter;
-        private final ObserverManager obManager;
-        private JDA jda;
+public class Bot {
+    protected static final Logger logger = LoggerFactory.getLogger(Bot.class);
+    private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private final EventWaiter waiter;
+    private final ObserverManager obManager;
+    private JDA jda;
+    private ConcurrentHashMap<String, Integer> channelNotExsitsCount;
 
     public Bot(EventWaiter waiter, ObserverManager obManager) {
         this.waiter = waiter;
         this.obManager = obManager;
+        channelNotExsitsCount = new ConcurrentHashMap<>();
     }
 
     public EventWaiter getWaiter() {
@@ -43,7 +42,7 @@ public class Bot
         this.jda = jda;
     }
 
-    public void shutdown(){
+    public void shutdown() {
         logger.info("shutdown ...");
         obManager.shutdown();
         jda.shutdown();
@@ -51,38 +50,47 @@ public class Bot
         System.exit(0);
     }
 
-    public void messageConsumerStart(){
-        Thread t = new Thread(()->{
+    public void messageConsumerStart() {
+        Thread t = new Thread(() -> {
             BlockingQueue<MessageDTO> queue = SingleBlockingQueue.getInstance();
-            while (true){
+            while (true) {
                 try {
                     MessageDTO dto = queue.take();
-                    Optional.ofNullable(jda.getTextChannelById(dto.channelId)).map(channel-> {
+                    Optional.ofNullable(jda.getTextChannelById(dto.channelId)).map(channel -> {
                         Message msg = dto.messageBuilder.build();
                         channel.sendMessage(msg)
-                                .queue(suc->logger.info("发送成功："+msg.getEmbeds().get(0).getDescription()), fail->logger.info("发送失败："+msg.getEmbeds().get(0).getDescription()));
+                                .queue(suc -> logger.info("发送成功：" + msg.getEmbeds().get(0).getDescription()), fail -> logger.info("发送失败：" + msg.getEmbeds().get(0).getDescription()));
                         return CommandResults.SUCCESSED;
-                    }).orElseGet(()->{
-                        getObManager().deleteChannel(dto.channelId);
+                    }).orElseGet(() -> {
                         return CommandResults.CHANNEL_NOT_EXISTS;
                     });
                 } catch (Exception e) {
-                    logger.warn("MessageDTO消费者：",e);
+                    logger.warn("MessageDTO消费者：", e);
                 }
             }
         });
         t.setDaemon(true);
         t.start();
     }
-    public void channelReaperStart(){
-        executor.scheduleAtFixedRate(()->{
-            getObManager().getChannels().forEach(channelId->{
-                        if(jda.getTextChannelById(channelId)==null) {
-                            logger.info("删除不存在的频道："+channelId);
-                            getObManager().deleteChannel(channelId);
+
+    public void channelReaperStart() {
+        executor.scheduleAtFixedRate(() -> {
+            getObManager().getChannels().forEach(channelId -> {
+                        if (jda.getTextChannelById(channelId) == null) {
+                            int counts = channelNotExsitsCount.compute(channelId, (channel, count) -> count == null ? 1 : count + 1);
+                            logger.info("Channel:" + channelId + "不存在, 当前计数" + counts);
+                            if (counts > 36) { //连续6小时不存在
+                                logger.info("删除不存在的Channel：" + channelId);
+                                getObManager().deleteChannel(channelId);
+                            }
+                        }else {
+                            if(channelNotExsitsCount.containsKey(channelId)){
+                                channelNotExsitsCount.remove(channelId);
+                                logger.info("Channel:"+channelId+"获取成功，清空计数");
+                            }
                         }
                     }
             );
-        },0,10, TimeUnit.MINUTES);
+        },1,10, TimeUnit.MINUTES);
     }
 }
